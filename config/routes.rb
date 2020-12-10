d4ebd74512d1faf1ed6b9b0e708c958738e514ec
rhday <54642787+rhday@users.noreply.github.com>
2020-12-10 16:28:11 +0000

    added namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :transactions
-  resources :accounts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do 
+    namespace :v1 do
+      resources :transactions
+      resources :accounts
+    end 
+  end  
+
+  #namespacing routes lets the browser know where to look for the controller within the backend of the application.
+  
 end
+
+fetch('http://localhost:3000/api/v1/transactions')
