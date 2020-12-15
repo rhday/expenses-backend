cf45992eb03e5bdabb994eca131dae070afd520f
rhday <54642787+rhday@users.noreply.github.com>
2020-12-15 09:24:38 +0000

    built out index and create functions for Account

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,10 +1,17 @@
 class API::V1::AccountsController < ApplicationController
 
     def index
-        
+        @accounts = Account.all
+        render json: @accounts
     end 
 
     def create
+        @account = Account.new(account_params)
+        if @account.save
+            render json: @account 
+        else 
+            render json: {error: 'Error creating account.'}
+        end 
     end 
 
     def show
