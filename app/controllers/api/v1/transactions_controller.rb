3e1c108226004ead4e3feef8824f3de9422ebfec
rhday <54642787+rhday@users.noreply.github.com>
2020-12-15 09:48:35 +0000

    index and create functions build in Transaction Controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,21 +1,34 @@
 class API::V1::TransactionsController < ApplicationController
+
+    before_action :set_account
     
     def index
+       @transactions = @account.transactions
+       render json: @transactions
     end 
 
     def create
+        @transaction = Transaction.find(params[:id])
+        render json: @transaction
     end 
 
     def show
+        
     end 
 
     def destroy
+       
     end 
 
     private
 
-    def transaction_params
-
+    def set_account
+        @account Account.find(params[:account_id])
     end 
+
+    def transaction_params
+        #the params require(.require) an account and permit (.permit) the attributes specified 
+        params.require(:transaction).permit(:amount ,:account_id, :kind, :date, :description)
+    end  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do ## This tells the application where to look "app/controllers/api/v1"
     namespace :v1 do ## This tells the application where to look "app/controllers/api/v1"
-      resources :transactions
-      resources :accounts
+      resources :accounts do
+        resources :transactions #nesting the transaction resource within the accounts resource means that transactions can only be accessed through accounts
     end 
   end  
 
