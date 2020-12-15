22eb54e1aaa78a6c4cafd034ce94bef0074dfc17
rhday <54642787+rhday@users.noreply.github.com>
2020-12-15 15:15:11 +0000

    generated serializers

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do ## This tells the application where to look "app/controllers/api/v1"
       resources :accounts do
         resources :transactions #nesting the transaction resource within the accounts resource means that transactions can only be accessed through accounts
-    end 
-  end  
+      end 
+    end  
+  end 
 
   #namespacing routes lets the browser know where to look for the controller within the backend of the application.
 
